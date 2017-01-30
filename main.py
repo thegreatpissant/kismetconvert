5fb40bf569292b430d948dda953ef2835ad67508
James A. Feister <thegreatpissant@gmail.com>
2017-01-30 01:31:28 -0500

    Cleanup output for csv

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ def main():
     groot = gpsxmlfile.getroot()
     nroot = netxmlfile.getroot()
     gpsssid = {}
-    print groot
-    print nroot
-    print 'Parsing gps'
+    #print groot
+    #print nroot
+    #print 'Parsing gps'
     for i in groot[1:]:
         if i.attrib["bssid"] and not i.attrib["bssid"] in gpsssid:
             gpsssid[i.attrib["bssid"]] = {'lat':i.attrib["lat"], 'lon':i.attrib["lon"], 'name':'NULL'}
-            print "b type: " , type(i.attrib["bssid"])
+            #print "b type: " , type(i.attrib["bssid"])
 
-    print "printing items"
-    for i in gpsssid.keys():
-        print "bssid: " , i,  " lat:", gpsssid[i]['lat'], ' lon:', gpsssid[i]['lon']
+    #print "printing items"
+    #for i in gpsssid.keys():
+        #print "bssid: " , i,  " lat:", gpsssid[i]['lat'], ' lon:', gpsssid[i]['lon']
 
-
-    print 'Parsing net'
+    #print 'Parsing net'
     w = nroot.findall('wireless-network')
     for i in w:
         if i.tag == "wireless-network":
             b = i.findall('BSSID')
-            print "bssid:" , b[0].text
+            #print "bssid:" , b[0].text
             e = i.findall('SSID')
             if len(e) == 0:
                 continue
             else:
                 es = e[0].findall('essid')
-                print "essid: " , es[0].text
-            print "BTYPE: " , type(b[0].text)
+                #print "essid: " , es[0].text
+            #print "BTYPE: " , type(b[0].text)
             if b[0].text in gpsssid:
                 gpsssid[b[0].text]["name"] = es[0].text
 
-    print "printing items"
+    #print "printing items"
     print "name,desc,latitude,longitude"
     for i in gpsssid.keys():
-        print '"',gpsssid[i]['name'], '",', i, ',', gpsssid[i]['lat'], ',', gpsssid[i]['lon']
+	print "\"{}\",{},{},{}".format(gpsssid[i]['name'], i, gpsssid[i]['lat'], gpsssid[i]['lon'])
+        #print '"',str(gpsssid[i]['name']).strip(), '",', i, ',', gpsssid[i]['lat'], ',', gpsssid[i]['lon']
 
 
 
@@ -88,7 +88,7 @@ if __name__ == '__main__':
         print errorCode
         sys.exit(2)
 
-    print "GPSxml file:", gfile
-    print "NETxml file:", nfile
+    #print "GPSxml file:", gfile
+    #print "NETxml file:", nfile
 
     main()
